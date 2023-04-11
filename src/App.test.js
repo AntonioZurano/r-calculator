876ec54b331e918d5337f47832547a57fb4ea12c
Antonio Zurano <azurano@outlook.es>
2023-04-11 19:11:06 +0200

    Comentar App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
+// Importamos las librerías necesarias para realizar las pruebas.
 import { render, screen } from '@testing-library/react';
+
+// Importamos el componente a testear.
 import App from './App';
 
+// Definimos la prueba.
 test('renders learn react link', () => {
+
+  // Hacemos el renderizado del componente App.
   render(<App />);
+
+  // Obtenemos el elemento que contiene el texto 'learn react'.
   const linkElement = screen.getByText(/learn react/i);
+
+  // Verificamos que el elemento obtenido existe en el documento.
   expect(linkElement).toBeInTheDocument();
 });
