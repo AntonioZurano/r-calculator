1354819b53f259ae2b87681c2ac42efc54b2d1dd
Antonio Zurano <azurano@outlook.es>
2023-04-11 19:15:19 +0200

    comentarios boton.js

--- src/Componentes/Boton/Boton.js
+++ src/Componentes/Boton/Boton.js
@@ -1,13 +1,22 @@
+//importamos React y el archivo de estilos Boton.css.
 import React from "react";
 import "./Boton.css";
+
+//declaramos la función Boton que recibe como parámetros los hijos y la función manejadora de clics.
 function Boton({ children, manejarClic}){
+
+    //declaramos la función esOperador que verifica si el valor es un operador y lo retorna.
     const esOperador = (value) =>{
-        return isNaN(value) && value!=="=" && value!=="." ? "operador" : "";
+        return isNaN(value) && value !== "=" && value !== "." ? "operador" : "";
     }
+
+    //retornamos el componente Boton con las propiedades className y onClick.
     return(
         <div className={`boton_cal ${esOperador(children)}`.trim()} onClick={() =>{manejarClic(children)}}>
             {children}
         </div>
     );
 }
+
+//exportamos la función Boton como componente default.
 export default Boton;
